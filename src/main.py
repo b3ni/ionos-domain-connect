3ac55021c7bbdb1f3b2b12abc431c69e41641a93
Benito Rodríguez <brarcos@gmail.com>
2022-08-08 20:31:56 +0200

    interval update

--- src/main.py
+++ src/main.py
@@ -1,19 +1,24 @@
 import os
+import subprocess
 from datetime import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 
+INTERVAL_UPDATE = int(os.getenv("INTERVAL_UPDATE", 60))
+
 scheduler = BlockingScheduler()
 
 
-@scheduler.scheduled_job(IntervalTrigger(seconds=20))
+@scheduler.scheduled_job(IntervalTrigger(seconds=INTERVAL_UPDATE))
 def train_model():
     print("[%s] Update all domains ..." % datetime.now())
     os.system("domain-connect-dyndns --config /config.json update --all")
 
 
 try:
-    print("[%s] Starting updater ..." % datetime.now())
+    print(
+        "[%s] Starting updater (period %s sec.) ..." % (datetime.now(), INTERVAL_UPDATE)
+    )
     scheduler.start()
 except (KeyboardInterrupt, SystemExit):
     print("[%s] Stop updater ..." % datetime.now())
